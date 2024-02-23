26af83f8fa388e5527de51cb4a31390a32879001
Abhishek Guleliya <126302855+AbhishekGuleliya@users.noreply.github.com>
2024-02-23 13:43:33 +0530

    Add files via upload

---
+++ Untitled-10.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+    const map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+};
+
+for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+        stack.push(s[i]);
+    } else {
+        const top = stack.pop();
+        if (s[i] !== map[top]) {
+            return false;
+        }
+    }
+}
+
+return stack.length === 0;
+};
+
+module.exports = { isValid };
